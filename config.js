c7d8ac37f01a51765da56aacbdfaec79af283cef
vr000m <vr000m.devel@gmail.com>
2014-12-09 05:04:40 -0500

    adding changes from our test server

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 var config = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'fazer.dialogue.io',
         //anonymousdomain: 'guest.example.com',
-        muc: 'conference.jitsi-meet.example.com', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.jitsi-meet.example.com', // FIXME: use XEP-0030
+        muc: 'conference.fazer.dialogue.io', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.fazer.dialogue.io', // FIXME: use XEP-0030
         //call_control: 'callcontrol.jitsi-meet.example.com'
     },
 //  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
 //  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
 //  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
-    clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
+    bosh: '//fazer.dialogue.io/http-bind', // FIXME: use xep-0156 for that
+    clientNode: 'http://jitsi.org/jitsimeet?via=callstats.io', // The name of client node advertised in XEP-0115 'c' stanza
     //defaultSipNumber: '', // Default SIP number
-    desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
-    chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
-    desktopSharingSources: ['screen', 'window'],
-    minChromeExtVersion: '0.1', // Required version of Chrome extension
+    desktopSharing: 'false', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
+    //chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
+    //desktopSharingSources: ['screen', 'window'],
+    //minChromeExtVersion: '0.1', // Required version of Chrome extension
     enableRtpStats: true, // Enables RTP stats processing
     openSctp: true, // Toggle to enable/disable SCTP channels
     channelLastN: -1, // The default value of the channel attribute last-n.
